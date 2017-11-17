5e8b615f4afbbb219d200361e1c43e8b7079d157
Luka <luka.hyli@gmail.com>
2017-11-17 17:49:34 +0100

    NetFramework to NetCore test project conversion

--- NetPractices.Cqrs.Tests/Properties/AssemblyInfo.cs
+++ NetPractices.Cqrs.Tests/Properties/AssemblyInfo.cs
@@ -2,11 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NetPractices.Cqrs.Tests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NetPractices.Cqrs.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -14,9 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("70008dbd-d78d-45f9-b512-c75e16e478b5")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
